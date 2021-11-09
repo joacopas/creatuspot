2e502f019d4910f0482c7fabb3b57ca91978ddcb
Joaquin Pastor <joaquinpastorsuiffet@gmail.com>
2021-11-09 14:23:12 +0100

    Correciones otrograficas + agregado de js

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// var nombre = prompt("Ingresa tu nombre:");
+// var apellido = prompt("Ingrese su apellido:");
+
+// if ((nombre != "") & (apellido != "")) {
+//   var confirmation = confirm(
+//     "Bienvenido/a " +
+//       nombre +
+//       " " +
+//       apellido +
+//       " a Crea tu spot" +
+//       "\nTe pediremos unos dato para conocer mejor a nuestros clientes."
+//   );
+//   if ((confirmation = false)) {
+//     alert("Bienvenido a Creatuspot");
+//   } else {
+//     var edad = prompt("Ingrese su edad:");
+//     var mes = prompt("Ingrese su mes de nacimiento:");
+//     var año = prompt("Ingrese su año de nacimiento");
+//   }
+//   if ((edad = "") & (mes = "") & (año = "")) {
+//     alert("Por favor vuelva ingrese datos correctamente");
+//   } else {
+//     alert(nombre + " " + apellido + "\nBienvenido a Creatuspot");
+//   }
+// } else {
+//   alert("Por favor ingrese su nombre y apellido");
+// }
+
+// if ((confirmation = false)) {
+//   alert("Bienvenido a Creatuspot");
+// } else {
+//   var edad = prompt("Ingrese su edad:");
+//   var mes = prompt("Ingrese su mes de nacimiento:");
+//   var año = prompt("Ingrese su año de nacimiento");
+// }
+// if ((edad = "") & (mes = "") & (año = "")) {
+//   alert("Por favor vuelva ingrese datos correctamente");
+// } else {
+//   alert("Bienvenido a Creatuspot");
+// }
